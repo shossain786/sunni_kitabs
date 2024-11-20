047ae035ab4cb05a76268a3f4ba76562dacf4578
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2024-11-20 23:44:35 +0530

    code updated

--- lib/screens/book_webview_screen.dart
+++ lib/screens/book_webview_screen.dart
@@ -7,6 +7,7 @@ class BookWebViewScreen extends StatefulWidget {
   const BookWebViewScreen(String link, {super.key, required this.bookUrl});
 
   @override
+  // ignore: library_private_types_in_public_api
   _BookWebViewScreenState createState() => _BookWebViewScreenState();
 }
 
